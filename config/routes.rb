aa4dbfe37be17057409e488591e4320ea8815cdf
Mio KS <capo_kaoru@yahoo.co.jp>
2015-02-09 10:42:46 +0000

    Add navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/biography'
-
-  get 'static_pages/discography'
-
-  get 'static_pages/songs'
-
-  get 'static_pages/shows'
-
-  get 'static_pages/contact'
-
+  
+  root  'static_pages#home'
+  match '/biography',    to: 'static_pages#biography',    via: 'get'
+  match '/discography',   to: 'static_pages#discography',   via: 'get'
+  match '/songs', to: 'static_pages#songs', via: 'get'
+  match '/shows',   to: 'static_pages#shows',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
